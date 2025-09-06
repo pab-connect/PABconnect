e934fefa5d995c73e508caca534c87b312cbf1a3
marcossilva11 <marcosant2170@gmail.com>
2025-09-06 13:41:43 -0300

    style: estilizar o footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer className="w-full bg-[#307039] text-white py-2 text-center">
       <p>&copy; 2025 Passa a Bola. Todos os direitos reservados.</p>
     </footer>
   )
