12991a1b147717b760889f4142b19009160ace60
marcossilva11 <marcosant2170@gmail.com>
2025-09-06 12:00:55 -0300

    style: adicionar estilos basicos do Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,36 @@
-import {Menu, X, Search, User} from 'lucide-react'
+import { Menu, X, Search, CircleUserRoundIcon } from "lucide-react";
 
 export default function Header() {
   return (
-    <header>
-      <div>
-        <h1>PAB CONNECT</h1>
-        {/* Barra de pesquisa */}
-        <div>
-          <div>
-            <input type="text" name="busca" id="busca" placeholder="Pesquisar..." />
-            <Search />
-          </div>
-        </div>
+    <header className="w-full bg-[#307039] text-white p-4 flex items-center justify-between">
+      <h1 className="text-xl text-center leading-none">
+        PAB<span className="block text-4xl font-bold -mt-1">CONNECT</span>
+      </h1>
 
-        {/* Perfil */}
-        <div>
-          <button>
-            <User />
-          </button>
+      {/* Barra de pesquisa */}
+      <div className="hidden lg:flex flex-1 max-w-md mx-6">
+        <div className="flex items-center w-full bg-[#DAD0F0] rounded-lg px-3 py-1 text-[#705C9B] cursor-pointer hover:shadow-md hover:bg-[#cec4e4] focus:bg-[#cec4e4] transition-all duration-300 ease-in-out">
+          <input
+            type="text"
+            name="busca"
+            id="busca"
+            className="flex-1 outline-none bg-transparent"
+          />
+          <Search className="w-5 h-5" />
         </div>
+      </div>
 
-        {/* Mobile: botao de menu */}
+      {/* Perfil */}
+      <div className="hidden lg:flex items-center">
         <button>
-          <Menu />
+          <CircleUserRoundIcon className="w-10 h-10 text-[#DAD0F0]" />
         </button>
       </div>
+
+      {/* Mobile: botao de menu */}
+      <button className="lg:hidden">
+        <Menu className="w-7 h-7 text-[#DAD0F0]" />
+      </button>
     </header>
-  )
+  );
 }
