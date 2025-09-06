c5918f1e859aca7eae40b9df8b49a8ed58be597d
marcossilva11 <marcosant2170@gmail.com>
2025-09-06 17:08:47 -0300

    feat: adicionar link para os botoes de redes sociais

--- src/components/RedesSociais/RedesSociais.jsx
+++ src/components/RedesSociais/RedesSociais.jsx
@@ -4,21 +4,32 @@ export default function RedesSociais() {
   return (
     <div className="flex flex-col items-center gap-2">
       <h3 className="text-2xl text-bold">Acompanhe nas redes</h3>
-      <div className="bg-white text-[#307039] p-2 rounded-lg text-lg cursor-pointer shadow-md hover:scale-103 transition duration-300 my-1 flex items-center gap-2">
-        <FaInstagram />
-        <span>@passaabola</span>
+      <div className="bg-white text-[#307039] p-2 rounded-lg text-lg cursor-pointer shadow-md hover:scale-103 transition duration-300 my-1">
+        <a
+          className="flex items-center gap-2"
+          target="_blank" href="https://www.instagram.com/passaabola"
+        >
+          <FaInstagram />
+          <span>@passaabola</span>
+        </a>
       </div>
-      <div className="bg-white text-[#307039] p-2 rounded-lg text-lg cursor-pointer shadow-md hover:scale-103 transition duration-300 my-1 flex items-center gap-2">
-        <FaWhatsapp />
-        <span>00291034791</span>
+      <div className="bg-white text-[#307039] p-2 rounded-lg text-lg cursor-pointer shadow-md hover:scale-103 transition duration-300 my-1 ">
+        <a target="_blank" href="https://whatsapp.com/channel/0029Vavm10347XeEyTTNi91i" className="flex items-center gap-2">
+          <FaWhatsapp />
+          <span>00291034791</span>
+        </a>
       </div>
-      <div className="bg-white text-[#307039] p-2 rounded-lg text-lg cursor-pointer shadow-md hover:scale-103 transition duration-300 my-1 flex items-center gap-2">
-        <FaTiktok />
-        <span>@passaabola</span>
+      <div className="bg-white text-[#307039] p-2 rounded-lg text-lg cursor-pointer shadow-md hover:scale-103 transition duration-300 my-1">
+        <a target="_blank" href="https://www.tiktok.com/@passabola" className="flex items-center gap-2">
+          <FaTiktok />
+          <span>@passaabola</span>
+        </a>
       </div>
-      <div className="bg-white text-[#307039] p-2 rounded-lg text-lg cursor-pointer shadow-md hover:scale-103 transition duration-300 my-1 flex items-center gap-2">
-        <FaYoutube />
-        <span>@passaabola</span>
+      <div className="bg-white text-[#307039] p-2 rounded-lg text-lg cursor-pointer shadow-md hover:scale-103 transition duration-300 my-1">
+        <a target="_blank" href="https://www.youtube.com/@passabola" className="flex items-center gap-2">
+          <FaYoutube />
+          <span>@passaabola</span>
+        </a>
       </div>
     </div>
   );
