30c9835829c4b2febee3375e353d7afd1011d80a
marcossilva11 <marcosant2170@gmail.com>
2025-09-06 12:39:14 -0300

    style: estilizar o Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,38 +1,48 @@
-import { Search, CircleUserRoundIcon, Settings } from "lucide-react";
+import { Search, CircleUserRoundIcon, Settings, House, UsersRound, MessagesSquare, Bell } from "lucide-react";
 
 export default function Sidebar() {
   return (
-    <aside>
+    <aside className="bg-[#314334] text-white p-4">
       {/* Mobile: barra de pesquisa */}
-      <div>
-        <div>
-          <input
-            type="text"
-            name="busca"
-            id="busca"
-          />
-          <Search />
-        </div>
+
+      <div className="mb-4 flex items-center w-full bg-[#DAD0F0] rounded-lg px-3 py-1 text-[#705C9B] cursor-pointer hover:shadow-md hover:bg-[#cec4e4] focus:bg-[#cec4e4] transition-all duration-300 ease-in-out">
+        <input
+          type="text"
+          name="busca"
+          id="busca"
+          className="flex-1 outline-none bg-transparent"
+        />
+        <Search className="w-5 h-5" />
       </div>
 
       {/* Links navegacao */}
       <nav>
-        <ul>
-          <li><a href="#">Página inicial</a></li>
-          <li><a href="#">Conexões</a></li>
-          <li><a href="#">Mensagens</a></li>
-          <li><a href="#">Notificações</a></li>
-          <li><a href="#">Perfil</a></li>
+        <ul className="flex flex-col gap-2">
+          <li>
+            <a href="#" className="flex gap-2 font-semibold underline"><House /> Página inicial</a>
+          </li>
+          <li>
+            <a href="#" className="flex gap-2 font-semibold"><UsersRound /> Conexões</a>
+          </li>
+          <li>
+            <a href="#" className="flex gap-2 font-semibold"><MessagesSquare /> Mensagens</a>
+          </li>
+          <li>
+            <a href="#" className="flex gap-2 font-semibold"><Bell /> Notificações</a>
+          </li>
+          <li>
+            <a href="#" className="flex gap-2 font-semibold"><CircleUserRoundIcon /> Perfil</a>
+          </li>
         </ul>
       </nav>
 
       {/* Configuracoes */}
-      <div>
-        <button>
+      <div className="mt-6">
+        <button className="flex items-center gap-2 font-semibold">
           <Settings />
           <span>Configurações</span>
         </button>
       </div>
     </aside>
-  )
+  );
 }
