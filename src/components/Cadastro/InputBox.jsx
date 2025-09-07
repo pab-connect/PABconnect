b3c80a455657a64a0d44da77f286002b0735bd7b
ogustavxs <gustavopessoasillva@gmail.com>
2025-09-07 00:09:03 -0300

    feat: componentes de auxilio adicionados

---
+++ src/components/Cadastro/InputBox.jsx
@@ -0,0 +1,27 @@
+export default function InputBox({ label, placeholder, type }) {
+  // Define autocomplete automaticamente
+  const autoCompleteMap = {
+    text: "name",
+    email: "email",
+    password: "new-password",
+    number: "off"
+  };
+
+  const autoComplete = autoCompleteMap[type] || "off";
+
+  const numberProps =
+    type === "number" ? { step: 1, min: 0 } : {};
+
+  return (
+    <div className="flex flex-col items-start w-full text-[#4f3882]">
+      <label>{label}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        autoComplete={autoComplete}
+        className="border mt-1 rounded-lg focus:outline-[#281452] h-12 w-full px-5 placeholder:text-lg"
+        {...numberProps}
+      />
+    </div>
+  );
+}
