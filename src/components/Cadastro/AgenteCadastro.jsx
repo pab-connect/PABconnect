67761c7926e3236df4ff0d1ab7059515e66079b3
ogustavxs <gustavopessoasillva@gmail.com>
2025-09-07 01:33:32 -0300

    feat: pagina de auth para agentes criada e estilizada

--- src/components/Cadastro/AgenteCadastro.jsx
+++ src/components/Cadastro/AgenteCadastro.jsx
@@ -2,8 +2,12 @@ import { useState } from "react";
 import InputBox from "./InputBox";
 import logo from "/logo-text.png";
 import UploadAndDisplayImage from "./UploadAndDisplayImage";
+import CheckboxGroup from "./CheckboxGroup";
+import CadastroConcluido from "./CadastroConcluido";
 
-export default function JogadorCadastro() {
+
+export default function AgenteCadastro () {
+
   const estadosBR = [
     "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará",
     "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão",
@@ -17,7 +21,7 @@ export default function JogadorCadastro() {
 
   return (
     <div className="w-screen min-h-screen overflow-y-auto flex flex-col items-center font-(family-name:--font-poppins)">
-      <main className="bg-white w-full max-w-3xl rounded-xl overflow-x-hidden">
+      <main className="bg-white w-full max-w-3xl overflow-x-hidden">
         
         {/* Step 1: Informações Pessoais */}
         {step === 1 && (
@@ -28,7 +32,7 @@ export default function JogadorCadastro() {
               alt="" 
             />
             <h2 className="font-extrabold text-2xl text-center text-[#4f3882] mb-10 transition-colors duration-300 hover:text-[#281452]">
-              Cadastro de jogadora - Informações Pessoais
+              Cadastro de olheiro(a) - Informações Pessoais
             </h2>
             <form className="flex flex-col gap-4 text-center">
               <div className="flex flex-col gap-5 justify-center items-center mb-10 text-lg">
@@ -36,7 +40,18 @@ export default function JogadorCadastro() {
                 <InputBox required={true} label="Email*" placeholder="Ex: maria@gmail.com" type="email"/>
                 <InputBox required={true} label="Senha*" placeholder="" type="password"/>
                 <InputBox required={true} label="Confirmar senha*" placeholder="" type="password"/>
+                <InputBox label="Telefone/Whatsapp" placeholder="Ex: 1144443333" type="tel"/>
                 <InputBox required={true} label="Idade*" placeholder="Ex: 18" type="number"/>
+                <div className="flex flex-col items-start w-full text-[#4f3882]">
+                  <label>Gênero</label>
+                  <select className="border mt-1 rounded-lg focus:outline-[#281452] h-12 w-full px-5 placeholder:text-lg transition-all duration-200 hover:shadow-md">
+                    <option value="Prefiro-nao-responder" key="Prefiro-nao-responder">Prefiro não responder</option>
+                    <option value="Masculino" key="Masculino">Masculino</option>
+                    <option value="Feminino" key="Feminino">Feminino</option>
+                    <option value="Nao-binario" key="Nao-binario">Não-binário</option>
+                    <option value="Outro" key="Outro">Outro</option>
+                  </select>
+                </div>
                 <div className="flex flex-col items-start w-full text-[#4f3882]">
                   <label>Estado*</label>
                   <select className="border mt-1 rounded-lg focus:outline-[#281452] h-12 w-full px-5 placeholder:text-lg transition-all duration-200 hover:shadow-md">
@@ -50,7 +65,7 @@ export default function JogadorCadastro() {
               </div>
               <div className="flex gap-4 w-full">
                 <button
-                  type="submit"
+                  type="button"
                   className="h-14 w-full bg-[#307039] text-white font-semibold rounded-lg text-lg cursor-pointer transition-all duration-300 hover:bg-[#24582b] hover:scale-105"
                   onClick={() => setStep(2)}
                 >
@@ -61,7 +76,7 @@ export default function JogadorCadastro() {
           </section>
         )}
 
-        {/* Step 2: Informações Esportivas */}
+        {/* Step 2: Informações profissionais */}
         {step === 2 && (
           <section className="w-full flex flex-col justify-center items-center p-10">
             <img 
@@ -70,21 +85,33 @@ export default function JogadorCadastro() {
               alt="" 
             />
             <h2 className="font-extrabold text-2xl text-center text-[#4f3882] mb-10 transition-colors duration-300 hover:text-[#281452]">
-              Cadastro de jogadora - Dados Atléticos
+              Cadastro de olheiro(a) - Informações profissionais
             </h2>
             <form className="flex flex-col gap-4 text-center">
               <div className="flex flex-col gap-5 justify-center items-center mb-10 text-lg">
-                <InputBox required={true} label="Posição*" placeholder="Ex: Atacante" type="text"/>
-                <InputBox label="Clube" placeholder="Ex: Corinthians" type="text"/>
-                <InputBox required={true} label="Altura*" placeholder="Ex: 165cm" type="number"/>
+                <InputBox required={true} label="Cargo/Função atual*" placeholder="Ex: Treinador" type="text"/>
+                <InputBox label="Clube ou organização associada" placeholder="Ex: Corinthians" type="text"/>
                 <div className="flex flex-col items-start w-full text-[#4f3882]">
-                  <label>Pé dominante</label>
-                  <select className="border mt-1 rounded-lg focus:outline-[#281452] h-12 w-full px-5 placeholder:text-lg transition-all duration-200 hover:shadow-md">
-                    <option value="Direito" key="Direito">Direito</option>
-                    <option value="Esquerdo" key="Esquerdo">Esquerdo</option>
-                    <option value="Ambos" key="Ambos">Ambos</option>
-                  </select>
+                    <label>Tempo de experiência (anos)</label>
+                    <select className="border mt-1 rounded-lg focus:outline-[#281452] h-12 w-full px-5 placeholder:text-lg transition-all duration-200 hover:shadow-md">
+                        <option value="" key="default">Selecione</option>
+                        <option value="0" key="0">Nenhuma experiência</option>
+                        <option value="1" key="1">1 ano</option>
+                        <option value="2-3" key="2-3">2 a 3 anos</option>
+                        <option value="4-5" key="4-5">4 a 5 anos</option>
+                        <option value="6-10" key="6-10">6 a 10 anos</option>
+                        <option value="10+" key="10+">Mais de 10 anos</option>
+                    </select>
                 </div>
+
+                <CheckboxGroup label="Categoria de atletas que busca" name='categorias' options={[
+                    { value: "Sub-13", label:"Sub-13" },
+                    { value: "Sub-15", label:"Sub-15" },
+                    { value: "Sub-17", label:"Sub-17" },
+                    { value: "Sub-20", label:"Sub-20" },
+                    { value: "Profissional", label:"Profissional" },
+                ]}/>
+                
               </div>
               <div className="flex gap-4 w-full">
                 <button
@@ -95,7 +122,7 @@ export default function JogadorCadastro() {
                   Voltar
                 </button>
                 <button
-                  type="submit"
+                  type="button"
                   className="h-14 w-1/2 bg-[#307039] text-white font-semibold rounded-lg text-lg cursor-pointer transition-all duration-300 hover:bg-[#24582b] hover:scale-105"
                   onClick={() => setStep(3)}
                 >
@@ -106,7 +133,7 @@ export default function JogadorCadastro() {
           </section>
         )}
 
-        {/* Step 3: Revisão ou Finalização */}
+        {/* Step 3: Redes e portfólio */}
         {step === 3 && (
           <section className="w-full flex flex-col justify-center items-center p-10">
             <img 
@@ -115,10 +142,19 @@ export default function JogadorCadastro() {
               alt="" 
             />
             <h2 className="font-extrabold text-2xl text-center text-[#4f3882] mb-10 transition-colors duration-300 hover:text-[#281452]">
-              Cadastro de jogadora - Mídia
+              Cadastro de jogadora - Redes e Portfólio
             </h2>
-            <p className="font-semibold text-[#4f3882] mb-5 text-lg">Foto de perfil</p>
+            <form className="flex flex-col gap-4 text-center">
+              <div className="flex flex-col gap-5 justify-center items-center mb-10 text-lg">
+            <InputBox label="Linkedin ou rede profissional" placeholder="Ex: linkedin.com/seuperfil" type="text"/>
+            <InputBox label="Site ou portfólio" placeholder="Ex: meuportfolio.com" type="text"/>
+            </div>
+            
+
+            <p className="font-semibold text-[#4f3882] text-lg">Foto de perfil</p>
+        
             <UploadAndDisplayImage />
+            </form>
             <p className="my-10 text-center text-lg transition-colors duration-300 hover:text-[#307039] cursor-pointer">
               Clique em finalizar para concluir o cadastro
             </p>
@@ -131,14 +167,17 @@ export default function JogadorCadastro() {
                 Voltar
               </button>
               <button
-                type="submit"
+                type="button"
                 className="h-14 w-1/2 bg-[#307039] text-white font-semibold rounded-lg text-lg cursor-pointer transition-all duration-300 hover:bg-[#24582b] hover:scale-105"
-              >
-                Finalizar
+                onClick={() => setStep(4)}
+                >
+                Finaliza
               </button>
+
             </div>
           </section>
         )}
+        {step === 4 && <CadastroConcluido />}
 
       </main>
     </div>
