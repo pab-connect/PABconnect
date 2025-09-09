f3720e12811539371bc2fc2f378bfcc826f0632e
Daniel Laureano <daniellaureanoluz@gmail.com>
2025-09-09 20:55:20 -0300

    feat: adicionar estrutura e estilização inicial da seção de notificações

---
+++ src/pages/Notificacoes.jsx
@@ -0,0 +1,9 @@
+export default function Notificacoes() {
+    return (
+        <div className="flex flex-1 bg-[#DAD0F0]  min-h-screen">
+            <div className="flex flex-1 flex-col items-center pt-[88px] p-4 mt-6 text-[#705C9B]">
+                <h2 className="text-3xl font-semibold mb-6">Notificações</h2>
+            </div>
+        </div>
+    )
+};
