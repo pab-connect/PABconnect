7ce2d9fab6db9965c671368a59f6694093648764
Daniel Laureano <daniellaureanoluz@gmail.com>
2025-09-09 00:39:22 -0300

    feat: adicionar e estilizar seção de criar post

--- src/pages/IndexJogadora.jsx
+++ src/pages/IndexJogadora.jsx
@@ -1,7 +1,10 @@
+import CriarPostIndexJogadora from "../components/CriarPostIndexJogadora/CriarPostIndexJogadora"
+
 export default function IndexJogadora() {
     return (
         <div className="flex flex-1 bg-[#DAD0F0]">
             <div className="flex flex-1 flex-col items-center pt-30 p-6 gap-5 lg:ml-64 lg:pt-30 lg:p-10">
+                <CriarPostIndexJogadora />
                 <hr className="w-full my-4 border-t-2 border-[#9f92bc]" />
             </div>
         </div>
