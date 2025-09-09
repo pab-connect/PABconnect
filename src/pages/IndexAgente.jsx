68c2ccc6069f335fe0fd15d19ab0532e0e10a023
Daniel Laureano <daniellaureanoluz@gmail.com>
2025-09-09 01:09:22 -0300

    importar seções no index do agente

--- src/pages/IndexAgente.jsx
+++ src/pages/IndexAgente.jsx
@@ -1,4 +1,6 @@
 import RadarTalentos from "../components/RadarTalentos"
+import PostUser from "../components/PostUser/PostUser"
+import PostMarta from "../components/PostMarta/PostMarta"
 
 export default function IndexAgente() {
     return (
@@ -6,6 +8,8 @@ export default function IndexAgente() {
             <div className="flex flex-1 flex-col items-center pt-30 p-6 gap-5 lg:ml-64 lg:pt-30 lg:p-10">
                 <RadarTalentos />
                 <hr className="w-full my-4 border-t-2 border-[#457c50]" />
+                <PostUser />
+                <PostMarta />
             </div>
         </div>
     )
