a3cfff0f372f8e702a6d0b4101ec3fa34c73781d
Daniel Laureano <daniellaureanoluz@gmail.com>
2025-09-09 01:04:27 -0300

    feat: adicionar e estilizar seção de radar de talentos

--- src/pages/IndexAgente.jsx
+++ src/pages/IndexAgente.jsx
@@ -1,7 +1,12 @@
+import RadarTalentos from "../components/RadarTalentos"
+
 export default function IndexAgente() {
     return (
-        <div className="flex flex-1 flex-col items-center pt-30 p-6 gap-5 lg:ml-64 lg:pt-30 lg:p-10">
-            <hr className="w-full my-4 border-t-2 border-[#457c50]" />
+        <div className="flex flex-1 bg-[#DAD0F0]">
+            <div className="flex flex-1 flex-col items-center pt-30 p-6 gap-5 lg:ml-64 lg:pt-30 lg:p-10">
+                <RadarTalentos />
+                <hr className="w-full my-4 border-t-2 border-[#457c50]" />
+            </div>
         </div>
     )
 }
