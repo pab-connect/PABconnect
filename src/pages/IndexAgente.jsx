8cddd68e9c03b988fd1fe21cdeee05017c9d090a
Daniel Laureano <daniellaureanoluz@gmail.com>
2025-09-09 00:58:28 -0300

    feat: adicionar estrutura inicial e responsividade no index do agente

---
+++ src/pages/IndexAgente.jsx
@@ -0,0 +1,7 @@
+export default function IndexAgente() {
+    return (
+        <div className="flex flex-1 flex-col items-center pt-30 p-6 gap-5 lg:ml-64 lg:pt-30 lg:p-10">
+            <hr className="w-full my-4 border-t-2 border-[#457c50]" />
+        </div>
+    )
+}
