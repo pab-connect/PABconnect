e18fd59cdb37f67410bf591dd2f527ac10dedebc
marcossilva11 <marcosant2170@gmail.com>
2025-09-13 14:42:22 -0300

    fix: exibir Header e Sidebar no index

--- src/pages/IndexJogadora.jsx
+++ src/pages/IndexJogadora.jsx
@@ -1,16 +1,20 @@
-import CriarPostIndexJogadora from "../components/CriarPostIndexJogadora/CriarPostIndexJogadora"
-import PostUser from "../components/PostUser/PostUser"
-import PostMarta from "../components/PostMarta/PostMarta"
+import CriarPostIndexJogadora from "../components/CriarPostIndexJogadora/CriarPostIndexJogadora";
+import PostUser from "../components/PostUser/PostUser";
+import PostMarta from "../components/PostMarta/PostMarta";
+import Header from "../components/Header/Header";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 export default function IndexJogadora() {
-    return (
-        <div className="flex flex-1 bg-[#DAD0F0]">
-            <div className="flex flex-1 flex-col items-center pt-30 p-6 gap-5 lg:ml-64 lg:pt-30 lg:p-10">
-                <CriarPostIndexJogadora />
-                <hr className="w-full my-4 border-t-2 border-[#9f92bc]" />
-                <PostUser />
-                <PostMarta />
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-1 bg-[#DAD0F0]">
+      <Header />
+      <div className="flex flex-1 flex-col items-center pt-30 p-6 gap-5 lg:ml-64 lg:pt-30 lg:p-10">
+        <Sidebar isDesktop={true} />
+        <CriarPostIndexJogadora />
+        <hr className="w-full my-4 border-t-2 border-[#9f92bc]" />
+        <PostUser />
+        <PostMarta />
+      </div>
+    </div>
+  );
 }
