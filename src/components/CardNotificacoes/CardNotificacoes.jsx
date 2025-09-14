599819850e03353405ecef99ee6908f5ec39733f
Daniel Laureano <daniellaureanoluz@gmail.com>
2025-09-14 00:34:04 -0300

    feat: adicionar header, sidebar e footer

--- src/components/CardNotificacoes/CardNotificacoes.jsx
+++ src/components/CardNotificacoes/CardNotificacoes.jsx
@@ -4,7 +4,7 @@ import {
 
 export default function CardNotificacoes({titulo, descricao}) {
     return (
-        <div className="flex flex-col gap-3 md:gap-2 lg:gap-1 p-4 lg:p-3 mb-2 mx-3 lg:mx-5 shadow-md rounded-lg bg-white">
+        <div className="flex flex-col gap-3 md:gap-2 lg:gap-1 p-4 lg:p-3 mb-3 mx-3 lg:mx-5 shadow-md rounded-lg bg-white">
         <div className='flex gap-1'>
             <Info className='md:w-6 md:h-7 lg:w-6 lg:h-7'/>
             <h3 className="text-base md:text-xl lg:text-lg font-bold">{titulo}</h3>
