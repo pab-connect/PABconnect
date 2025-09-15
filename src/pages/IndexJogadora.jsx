a6089dd78c0b5ccc9cd9af75513252cb1a30ca3d
ogustavxs <gustavopessoasillva@gmail.com>
2025-09-14 21:02:09 -0300

    feat: agora os posts da api são mostrados na page inicial

--- src/pages/IndexJogadora.jsx
+++ src/pages/IndexJogadora.jsx
@@ -3,8 +3,41 @@ import PostUser from "../components/PostUser/PostUser";
 import PostMarta from "../components/PostMarta/PostMarta";
 import Header from "../components/Header/Header";
 import Sidebar from "../components/Sidebar/Sidebar";
+import { getAll, API_POSTS_URL, API_BASE_URL } from "../services/apiService";
+import { useEffect, useState } from "react";
+
 
 export default function IndexJogadora() {
+  const [posts, setPosts] = useState([]);
+  const [jogadoras, setJogadoras] = useState([]);
+  const userLogadoEmail = JSON.parse(localStorage.getItem("user"))?.email;
+  const usuarioLogado = jogadoras.find(u => u.email === userLogadoEmail)
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const users = await getAll(API_BASE_URL, "jogadoras");
+        setJogadoras(users);
+      } catch (error) {
+        console.error("Erro ao buscar jogadoras:", error);
+      }
+    }
+
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const posts = await getAll(API_POSTS_URL, "posts");
+        setPosts(posts);
+      } catch (error) {
+        console.error("Erro ao buscar jogadoras:", error);
+      }
+    }
+
+    fetchPosts();
+  }, []);
   return (
     <div className="flex flex-1 bg-[#DAD0F0]">
       <Header />
@@ -12,7 +45,14 @@ export default function IndexJogadora() {
         <Sidebar isDesktop={true} />
         <CriarPostIndexJogadora />
         <hr className="w-full my-4 border-t-2 border-[#9f92bc]" />
-        <PostUser />
+        {posts.map((post) => (
+          <PostUser
+            key={post.id}
+            post={post}
+            usuario={jogadoras.find(u => u.id === post.usuario) || {}} // pega o dono do post
+            idUsuarioLogado={usuarioLogado?.id}
+          />
+        ))}
         <PostMarta />
       </div>
     </div>
