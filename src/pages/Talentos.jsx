79ce5e4aff7bd5880b5950fcff20f3c7d3c64f4c
marcossilva11 <marcosant2170@gmail.com>
2025-09-15 12:18:38 -0300

    style: ajustar numero de cards na tela

--- src/pages/Talentos.jsx
+++ src/pages/Talentos.jsx
@@ -90,7 +90,7 @@ export default function Talentos() {
             {/* cards */}
             <section
               id="jogadoras"
-              className="flex-1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4"
+              className="flex-1 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4"
             >
               {jogadorasFiltradas.map((j) => (
                 <CardTalentos
