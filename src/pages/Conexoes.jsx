6f16b743e353e98f6c8c340fc1f83dfe073d6b06
Gabriel Speck <gabriel.speck@gmail.com>
2025-09-16 08:17:30 -0300

    TesteConexoes

---
+++ src/pages/Conexoes.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+const Conexoes = () => {
+    return(
+        <div>
+            <h1>Alguma Coisa</h1>
+        </div>
+    )
+}
+
+export default Conexoes
