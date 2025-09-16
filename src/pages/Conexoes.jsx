5a4a5c86174e6612097875350537f074466670bf
Gabriel Speck <gabriel.speck@gmail.com>
2025-09-16 14:01:22 -0300

    Card

--- src/pages/Conexoes.jsx
+++ src/pages/Conexoes.jsx
@@ -11,10 +11,14 @@ const Conexoes = () => {
                 <h1 className="text-4xl font-extrabold dark:text-[#705C9B] mb-8">Conexões</h1>
                 <input type="text" placeholder="Buscar por nome ou posição..." className="rounded-xl border-2 border-[#705C9B] bg-gray-200 mb-8 w-80 h-10"/>
                 <div className="rounded-xl border-2 border-[#705C9B] bg-gray-200 text-black p-6 w-100 font-mono text-lg grid grid-cols-4 gap-4 mb-8">
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>4</div>
+                    <div className="flex items-center gap-4 p-4">
+                        <img src="img.png" alt="" className="w-20 h-20"/>
+                        <div className="flex-1">
+                            <h2 className="text-lg font-semibold">Nome</h2>
+                            <p className="text-gray-600 text-sm">Zagueira | Clube Profissional Acreano</p>
+                        </div>
+                        <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Mensagens</button>
+                    </div>
                 </div>
             </div>
         </div>
