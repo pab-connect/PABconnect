de31da7ace1d5dd1c193e5785def40e33295cece
Gabriel Speck <gabriel.speck@gmail.com>
2025-09-16 16:03:08 -0300

    mensagens

--- src/pages/Mensagens.jsx
+++ src/pages/Mensagens.jsx
@@ -4,10 +4,18 @@ import Sidebar from "../components/Sidebar/Sidebar";
 
 const Mensagens = () => {
     return(
-        <div className="flex flex-1 bg-[#DAD0F0]">
+        <div className="bg-[#DAD0F0]">
             <Header/>
-            <div className="flex flex-1 flex-col items-center pt-30 p-6 gap-5 lg:ml-64 lg:pt-30 lg:p-10">
+            <div className=" flex flex-col items-center pt-30 p-6 gap-5 lg:ml-64 lg:pt-30 lg:p-10">
                 <Sidebar isDesktop={true} />
+                <div className="flex flex-row-2 justify-center gap-100">
+                    <div className="border-2 border-[#705C9B] bg-gray-200 h-auto w-100">
+                        <h1 className="text-4xl font-extrabold dark:text-[#705C9B] mb-8 flex flex-row justify-center">Mensagens</h1>
+                    </div>
+                    <div className="border-2 border-[#705C9B] bg-gray-200 h-auto w-100">
+                        <h1 className="text-4xl font-extrabold dark:text-[#705C9B] mb-8 flex flex-row justify-center">Nome</h1>
+                    </div>
+                </div>
             </div>
         </div>
     )
