cf2c0793984ad18741998364e36ad4eb0b55be9c
marcossilva11 <marcosant2170@gmail.com>
2025-09-28 14:19:13 -0300

    style: adicionar estilo ao overlay de carregamento

--- src/components/LoadingOverlay/LoadingOverlay.jsx
+++ src/components/LoadingOverlay/LoadingOverlay.jsx
@@ -2,10 +2,10 @@ import { LoaderCircle } from "lucide-react"
 
 export default function LoadingOverlay() {
   return (
-    <div>
-      <div>
-        <LoaderCircle />
-        <span>
+    <div className="absolute flex items-center justify-center bg-white/50 backdrop-blur-sm z-50">
+      <div className="flex flex-col items-center gap-3">
+        <LoaderCircle className="h-14 w-14 animate-spin" />
+        <span className="text-sm">
           Carregando...
         </span>
       </div>
