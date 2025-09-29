c2b95595a1ca0caec10c212648b80a523fbed6bd
ogustavxs <gustavopessoasillva@gmail.com>
2025-09-29 17:50:10 -0300

    feat: atualizado cards de eventos para aceitarem props

--- src/components/Eventos/CardEvento.jsx
+++ src/components/Eventos/CardEvento.jsx
@@ -1,29 +1,44 @@
 import { Calendar, MapPin, X, Check, Hourglass } from 'lucide-react';
 
-export default function CardEvento() {
+export default function CardEvento({ situacao="aberto", title, img, localization, date, description }) {
     return (
         <div className="grid grid-cols-1 auto-rows-auto gap-4 justify-items-center items-center bg-white p-6 rounded-lg">
-            <img src="https://www.portalcomunicare.com.br/wp-content/uploads/2023/07/Feminino_006.jpg" alt="" className="w-40 border-black border-2 rounded-lg"/>
+            <img src={img} alt="" className="w-40 border-black border-2 rounded-lg"/>
             <section className="">
                 <div className='flex flex-col items-center'>
-                    <h5 className='font-semibold'>Peneira Aberta Sub-17</h5>
-                    <div className='text-red-500 flex'>
-                        <X/>
-                        Encerrado
-                    </div>
+                    <h5 className='font-semibold'>{title}</h5>
+                    {situacao === "aberto" && 
+                        <div className='text-green-700 flex'>
+                            <Check/>
+                            Aberto
+                        </div>
+                    }
+                    {situacao === "andamento" && 
+                        <div className='text-yellow-400 flex'>
+                            <Hourglass/>
+                            Em andamento
+                        </div>
+                    }
+                    {situacao === "encerrado" && 
+                        <div className='text-red-500 flex'>
+                            <X/>
+                            Encerrado
+                        </div>
+                    }
                 </div>
                 <div className='flex gap-2 my-2'>
                     <MapPin className='text-[#307039]'/>
-                    Estádio Municipal de São Paulo, SP
+                    {localization}
                 </div>
                 <div className='flex gap-2 my-2'>
                     <Calendar className='text-[#307039]'/>
-                    11/02/2025 - 09:00
+                    {date}
                 </div>
-                <p>Venha participar da nossa peneira. As inscrições são gratuitas, com avaliação técnica e olheiros</p>
+                <p>{description}</p>
             </section>
-            <section className=''>
+            <section className='flex flex-col items-center gap-2'>
                 <button className='bg-[#307039] text-white px-6 py-2 rounded-lg'>Mais informações</button>
+                {situacao == "aberto" && <button className='bg-[#307039] text-white px-6 py-2 rounded-lg'>Inscrever-se</button>}
             </section>
         </div>
     )
--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -16,7 +16,11 @@ export default function Eventos() {
                     <section className="w-full flex flex-col gap-6">
                         <CollapsibleGroup/>
                         <Separator className={'bg-[#307039]'}/>
-                        <CardEvento/>
+                        <section className="grid gap-6">
+                            <CardEvento situacao="encerrado" title="Peneira Aberta Sub-17" img="https://www.portalcomunicare.com.br/wp-content/uploads/2023/07/Feminino_006.jpg" localization="Estádio Municipal de São Paulo, SP" date="11/02/2025 - 09:00" description="Venha participar da nossa peneira. As inscrições são gratuitas, com avaliação técnica e olheiros"/>
+                            <CardEvento situacao="andamento" title="Guerreiras FC vs. Real FC" img="https://i.ytimg.com/vi/QHXA059YBAo/maxresdefault.jpg" localization="Mercado Livre Arena Pacaembu - SP" date="16/02/2025 - 15:00" description="As Guerreiras FC enfrentam o Real FC em um amistoso que promete muita emoção e talento em campo. Essa é uma oportunidade de ouro para atletas mostrarem suas habilidades diante de olheiros e comissão técnica. Venha apoiar o futebol feminino de base e descobrir os novos talentos do esporte!"/>
+                            <CardEvento situacao="aberto" title="Copa Revelação Feminina Sub-20" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOs8OpB2vc71MDuDrIb74vORR6yi67lFvVXw&s" localization="Centro Esportivo Zona Sul – São Paulo, SP" date="22/02/2025 - 10:00" description="A Copa Revelação Feminina é um dos torneios mais aguardados do futebol de base. Reunindo equipes de todo o estado, o evento promove jogos de alto nível técnico, com olheiros convidados e cobertura da mídia esportiva local. Esta é uma grande chance de visibilidade para jovens talentos que sonham em ingressar no futebol profissional."/>
+                        </section>
                     </section>
                 </div>
             </div>
