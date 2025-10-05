f2fe0c804cfe83d25034d0067c591831689a3f1c
ogustavxs <gustavopessoasillva@gmail.com>
2025-10-05 17:54:14 -0300

    feat: adicionada funcionalidade de criação de eventos em conta de organizadores

--- src/components/Eventos/SelectElement.jsx
+++ src/components/Eventos/SelectElement.jsx
@@ -10,16 +10,16 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 
-export default function SelectElement({ title, placeholder, options, name, onChange }) {
+export default function SelectElement({ title, placeholder, options, name, onChange, className="padrao" }) {
   return (
     <Select onValueChange={(value) => onChange({ name, value })} name={name}>
-      <SelectTrigger className="w-full sm:max-w-50 min-h-12 md:shadow-gray-500 text-md bg-white">
+      <SelectTrigger className={className==="padrao" ? "w-full sm:max-w-50 min-h-12 md:shadow-gray-500 text-md bg-white" : className}>
         <SelectValue placeholder={placeholder} />
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
           <SelectLabel>{title}</SelectLabel>
-          <SelectItem value="todos" className={"truncate"}>Todos</SelectItem>
+          {className==="padrao" && <SelectItem value="todos" className={"truncate"}>Todos</SelectItem>}
           {options.map((option)=><SelectItem key={options.indexOf(option)+1} className={"truncate"} value={option.toLowerCase()}>{option.length > 25 ? option.slice(0, 25) + "..." : option}</SelectItem>)}
         </SelectGroup>
       </SelectContent>
--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -3,9 +3,10 @@ import { CollapsibleGroup } from "../components/Eventos/CollapsibleGroup";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import Sidebar from "../components/Sidebar/Sidebar";
-import CardEvento from "@/components/Eventos/CardEvento";
+import CardEvento from "../components/Eventos/CardEvento";
 import { useState, useEffect } from "react";
 import { getAll, API_POSTS_URL, API_BASE_URL } from "@/services/apiService";
+import CriarEventoDialog from "../components/DialogComponents/CriarEventoDialog";
 
 export default function Eventos() {
     const [eventos, setEventos] = useState([]);
@@ -58,9 +59,10 @@ export default function Eventos() {
                 <Sidebar isDesktop={true} />
                 <div className="flex flex-1 flex-col items-center p-4 px-8 mt-4 md:mt-6 lg:ml-64 text-[#705C9B]">
                     <h2 className="text-3xl md:text-5xl lg:text-4xl font-semibold md:self-start md:font-bold text-black mb-6">Eventos</h2>
-                    <section className="w-full flex flex-col gap-6 md:gap-4">
+                    <section className="w-full relative flex flex-col gap-6 md:gap-4">
                         <CollapsibleGroup key={100} formData={formData} setFormData={setFormData} localizacoes={localizacoes}/>
                         <Separator className={'bg-[#307039]'}/>
+                        <CriarEventoDialog/>
                         <section className="grid gap-6 justify-center md:grid-cols-1 sm:grid-cols-2">
                             {filtrados.length === 0 ? 
                             <p className="text-center text-gray-600">Nenhum evento encontrado.</p> :
